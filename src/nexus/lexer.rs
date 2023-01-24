f407576014eb825cd80cc7b29e09561fd8bafec8
Josh Seligman <joshua.seligman1@marist.edu>
2023-01-23 19:34:09 -0500

    Fixed the counting and empty tokens generated from the whitespace

--- src/nexus/lexer.rs
+++ src/nexus/lexer.rs
@@ -6,14 +6,19 @@ pub fn lex(source_code: String) {//-> Vec<Token> {
     // This represents all possible terminal characters for which to mark the end of the current search
     let terminal_chars = Regex::new(r"^\s$").unwrap();
 
+    // The line and column numbers in the file
     let mut line_number: usize = 1;
     let mut col_number: usize = 1;
 
+    // The start and end indices in the source code string for the token
+    // cur_start == best_end means that the token is empty (space or newline by itself)
     let mut cur_start: usize = 0;
     let mut best_end: usize = 0;
 
+    // The cur token (implementation may change)
     let mut cur_token: Token = Token::Unrecognized(String::from(""));
 
+    // The current position in the source code
     let mut trailer: usize = 0;
 
     // Iterate through the end of the string
@@ -31,31 +36,35 @@ pub fn lex(source_code: String) {//-> Vec<Token> {
             let cur_sub: &str = &source_code[cur_start..trailer + 1];
             
             if upgrade_token(cur_sub, &mut cur_token) {
-                best_end = trailer.to_owned();
+                best_end = trailer + 1;
             }
         } else {
-            nexus_log::log(String::from("LEXER"), format!("Found {:?} at ({}, {})", cur_token, line_number, col_number));
+            if best_end - cur_start > 0 {
+                // There is a token of some kind
+                nexus_log::log(String::from("LEXER"), format!("Found {:?} at ({}, {})", cur_token, line_number, col_number));
 
-            if cur_char.eq("\n") {
-                // Increment line number and reset column number
-                line_number += 1;
-                col_number = 1;
+                // Update the column number to accommodate the length of the token
+                col_number += best_end - cur_start;
+
+                // Move the trailer to the best end - 1 (will get incremented at the loop bottom)
+                trailer = best_end - 1;
+                // Move cur_start to the beginning of the next possible token
+                cur_start = trailer + 1;
+
+                cur_token = Token::Unrecognized(String::from(""));
             } else {
-                // Column number gets increased by the length of the token + 2
-                // (1 for the space and 1 for best_end - cur_start being 1 less than the length of the token)
-                col_number += best_end - cur_start + 2;
+                // Token is empty
+                cur_start += 1;
+                best_end += 1;
+
+                // New line should update the line and column numbers
+                if cur_char.eq("\n") {
+                    line_number += 1;
+                    col_number = 1;
+                } else {
+                    col_number += 1;
+                }
             }
-
-            // If the best ending is not next to the terminating character, then move the trailer back to where it should start back up again
-            if best_end < trailer - 1 {
-                trailer = best_end.to_owned();
-            }
-            cur_start = trailer + 1;
-            best_end = trailer + 1;
-
-            debug!("{}", format!("NEW: trailer: {}, cur_start: {}, best_end: {}", trailer, cur_start, best_end));
-
-            cur_token = Token::Unrecognized(String::from(""));
         }
 
         trailer += 1;
@@ -65,12 +74,12 @@ pub fn lex(source_code: String) {//-> Vec<Token> {
 fn upgrade_token(substr: &str, best_token_type: &mut Token) -> bool {
     // Create the keywords
     let keywords: RegexSet = RegexSet::new(&[
-        r"if",
-        r"while",
-        r"print",
-        r"string",
-        r"int",
-        r"boolean"
+        r"^if$",
+        r"^while$",
+        r"^print$",
+        r"^string$",
+        r"^int$",
+        r"^boolean$"
     ]).unwrap();
 
     let identifiers = Regex::new(r"^[a-z]$").unwrap();
