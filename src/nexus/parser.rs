e3ac304b0699732bb0295297fef420d9d2d130f8
Josh Seligman <joshua.seligman1@marist.edu>
2023-02-07 19:11:14 -0500

    Fixed the infinite recursion for unmatched braces and decided on how I will handle errors in parse

--- src/nexus/parser.rs
+++ src/nexus/parser.rs
@@ -26,9 +26,32 @@ impl Parser {
         // Reset the index to be 0
         self.cur_token_index = 0;
 
+        let mut success: bool = true;
+
         // A program consists of a block followed by an EOP marker
         // First will check block and then the token
-        if self.parse_block(token_stream).is_err() || self.match_token(token_stream, TokenType::Symbol(Symbols::EOP)).is_err() {
+        let program_block_res: Result<(), String> = self.parse_block(token_stream);
+        if program_block_res.is_ok() {
+            let eop_res: Result<(), String> = self.match_token(token_stream, TokenType::Symbol(Symbols::EOP));
+            if eop_res.is_err() {
+                success = false;
+                nexus_log::log(
+                    nexus_log::LogTypes::Error,
+                    nexus_log::LogSources::Parser,
+                    eop_res.unwrap_err()
+                );
+            }
+        } else {
+            success = false;
+            nexus_log::log(
+                nexus_log::LogTypes::Error,
+                nexus_log::LogSources::Parser,
+                program_block_res.unwrap_err()
+            );
+        }
+
+
+        if !success {
             // Log that we are parsing the program
             nexus_log::log(
                 nexus_log::LogTypes::Error,
@@ -38,7 +61,7 @@ impl Parser {
         }
     }
 
-    fn parse_block(&mut self, token_stream: &Vec<Token>) -> Result<(), ()> {
+    fn parse_block(&mut self, token_stream: &Vec<Token>) -> Result<(), String> {
         // Log that we are parsing a block
         nexus_log::log(
             nexus_log::LogTypes::Debug,
@@ -49,25 +72,20 @@ impl Parser {
         // Check for left brace
         let lbrace_err: Result<(), String> = self.match_token(token_stream, TokenType::Symbol(Symbols::LBrace));
         if lbrace_err.is_err() {
-            nexus_log::log(
-                nexus_log::LogTypes::Error,
-                nexus_log::LogSources::Parser,
-                lbrace_err.unwrap_err()
-            );
-            return Err(());
+            // Return the error message if the left brace does not exist
+            return lbrace_err;
         }
 
-        self.parse_statement_list(token_stream);
+        let statement_list_res: Result<(), String> = self.parse_statement_list(token_stream);
+        if statement_list_res.is_err() {
+            return statement_list_res;
+        }
 
         // Check for right brace
         let rbrace_err: Result<(), String> = self.match_token(token_stream, TokenType::Symbol(Symbols::RBrace));
         if rbrace_err.is_err() {
-            nexus_log::log(
-                nexus_log::LogTypes::Error,
-                nexus_log::LogSources::Parser,
-                rbrace_err.unwrap_err()
-            );
-            return Err(());
+            // Return the error message if the right brace does not exist
+            return rbrace_err;
         }
 
         // Return ok if we have received everything that goes into a block
@@ -104,7 +122,7 @@ impl Parser {
         return Ok(());
     }
 
-    fn parse_statement_list(&mut self, token_stream: &Vec<Token>) {
+    fn parse_statement_list(&mut self, token_stream: &Vec<Token>) -> Result<(), String> {
         // Log that we are parsing a statement list
         nexus_log::log(
             nexus_log::LogTypes::Debug,
@@ -112,31 +130,60 @@ impl Parser {
             String::from("Parsing StatementList")
         );
 
-        // Make sure that the statement list is not empty
-        if !token_stream[self.cur_token_index].token_type.eq(&TokenType::Symbol(Symbols::RBrace)) {
-            self.parse_statement(token_stream);
-            if !token_stream[self.cur_token_index].token_type.eq(&TokenType::Symbol(Symbols::RBrace)) {
-                self.parse_statement_list(token_stream);
+        // Make sure that the statement list is not empty (for programs that are {}$)
+        if self.cur_token_index < token_stream.len() && !token_stream[self.cur_token_index].token_type.eq(&TokenType::Symbol(Symbols::RBrace)) {
+            // Parse the statement
+            let statement_res: Result<(), String> = self.parse_statement(token_stream);
+            if statement_res.is_err() {
+                return statement_res;
+            } else if self.cur_token_index < token_stream.len() && !token_stream[self.cur_token_index].token_type.eq(&TokenType::Symbol(Symbols::RBrace)) {
+                // StatementList = Statement StatementList, so call parse on the next statement list
+                return self.parse_statement_list(token_stream);
+            }  else {
+                // There is no more to print, so 
+                return Ok(());
             }
+        } else {
+            return Ok(());
         }
     }
 
-    fn parse_statement(&mut self, token_stream: &Vec<Token>) {
+    fn parse_statement(&mut self, token_stream: &Vec<Token>) -> Result<(), String> {
         // Log that we are parsing a statement
         nexus_log::log(
             nexus_log::LogTypes::Debug,
             nexus_log::LogSources::Parser,
             String::from("Parsing Statement")
         );
+
+        // Assume we are ok
+        let mut statement_res: Result<(), String> = Ok(());
+
         match &token_stream[self.cur_token_index].token_type {
+            // Print statements
             TokenType::Keyword(Keywords::Print) => self.parse_print_statement(token_stream),
+
+            // Assignment statements
             TokenType::Identifier(_) => {}, // Parse assignment statement
+
+            // VarDecl statements
             TokenType::Keyword(Keywords::Int) | TokenType::Keyword(Keywords::String) | TokenType::Keyword(Keywords::Boolean) => {}, // Parse var declaration
+
+            // While statements
             TokenType::Keyword(Keywords::While) => {}, // Parse while statement
+
+            // If statements
             TokenType::Keyword(Keywords::If) => {},// Parse if statement,
-            TokenType::Symbol(Symbols::LBrace) => self.parse_block(token_stream).unwrap(),
-            _ => {} // Figure out something to do to end the recursion
+
+            // Block statements
+            TokenType::Symbol(Symbols::LBrace) => statement_res = self.parse_block(token_stream),
+
+            // Invalid statement starter tokens
+            _ => {
+                statement_res = Err(format!("Invalid statement token [ {:?} ] at {:?}; Valid statement beginning tokens are {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}", token_stream[self.cur_token_index].token_type, token_stream[self.cur_token_index].position, TokenType::Keyword(Keywords::Print), TokenType::Identifier(String::from("a-z")), TokenType::Keyword(Keywords::Int), TokenType::Keyword(Keywords::String), TokenType::Keyword(Keywords::Boolean), TokenType::Keyword(Keywords::While), TokenType::Keyword(Keywords::If), TokenType::Symbol(Symbols::LBrace)));
+            }
         }
+        return statement_res;
     }
 
     fn parse_print_statement(&mut self, token_stream: &Vec<Token>) {//-> Result<bool, ()> {
