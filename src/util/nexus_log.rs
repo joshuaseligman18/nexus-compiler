b3f76c877729d78030f87ae33da0b60b54cca0bb
Josh Seligman <joshua.seligman1@marist.edu>
2023-02-23 10:18:50 -0500

    Made logs more colorful

--- src/util/nexus_log.rs
+++ src/util/nexus_log.rs
@@ -25,76 +25,73 @@ pub enum LogSources {
 
 // Function that logs a message with the given type and source
 pub fn log(log_type: LogTypes, src: LogSources, msg: String) {
+    // Get the log area
+    let log_area: Element = get_log_area();
+
+    // Create the new element to place in the logs
+    let new_log: Element = get_document().create_element("p").expect("Should be able to create the element");
+    new_log.set_inner_html(format!("[{} - {}]: {}", log_type, src, msg).as_str());
+
+    // Set the new value
+    log_area.append_child(&new_log).expect("Should be able to add the child");
+
+    // Special cases and such
     match log_type {
         LogTypes::Debug => {
             // Only log if in verbose mode
-            if is_verbose_mode(&src) {
-                // Get the log area
-                let log_area: HtmlTextAreaElement = get_log_area();
-
-                // Get the original value
-                let mut log_value: String = log_area.value();
-
-                // Add the new message to the logs
-                log_value.push_str(format!("[{} - {}]: {}\n", log_type, src, msg).as_str());
-
-                // Set the new value
-                log_area.set_value(&log_value);
+            if !is_verbose_mode(&src) {
+                log_area.remove_child(&new_log).expect("Should be able to remove the child");
             }
         },
+        LogTypes::Error => {
+            // Errors have special classes
+            new_log.set_class_name("error");
+        },
+        LogTypes::Warning => {
+            // Set the warning class
+            new_log.set_class_name("warning");
+        },
         _ => {
-            // Everything else is logged unconditionally
-            // Get the log area
-            let log_area: HtmlTextAreaElement = get_log_area();
-
-            // Get the original value
-            let mut log_value: String = log_area.value();
-
-            // Add the new message to the logs
-            log_value.push_str(format!("[{} - {}]: {}\n", log_type, src, msg).as_str());
-
-            // Set the new value
-            log_area.set_value(&log_value);
+            // Nothing else to do here
         }
     }
 }
 
 pub fn insert_empty_line() {
     // Get the log area
-    let log_area: HtmlTextAreaElement = get_log_area();
+    let log_area: Element = get_log_area();
 
-    // Get the original value
-    let mut log_value: String = log_area.value();
-
-    // Add the new message to the logs
-    log_value.push_str("\n");
-
-    // Set the new value
-    log_area.set_value(&log_value);
+    // The new line is just a br tag
+    let new_line: Element = get_document().create_element("br").expect("Should be able to create the br element");
+    log_area.append_child(&new_line).expect("Should be able to add the child");
 }
 
 // Function to clean the logs
 pub fn clear_logs() {
     // Get the log area
-    let log_area: HtmlTextAreaElement = get_log_area();
+    let log_area: Element = get_log_area();
 
-    // Set the value to an empty string
-    log_area.set_value("");
+    // Remove all children by wiping the inner html
+    log_area.set_inner_html("");
 }
 
-fn get_log_area() -> HtmlTextAreaElement {
+fn get_log_area() -> Element {
+    let document: Document = get_document();
+
+    // Get the area where the logs are printed
+    let log_area: Element = document
+        .get_element_by_id("nexus-log-area")
+        .expect("There should be a nexus-log-area element");
+
+    return log_area;
+}
+
+fn get_document() -> Document {
     // Grab the window and document elements for DOM manipulation
     let window: Window = web_sys::window().expect("The window object should exist.");
     let document: Document = window.document().expect("The document object should exist");
 
-    // Get the textarea where the logs are printed
-    let log_area: HtmlTextAreaElement = document
-        .get_element_by_id("nexus-log-area")
-        .expect("There should be a nexus-log-area element")
-        .dyn_into::<HtmlTextAreaElement>()
-        .expect("The element should be recognized as a textarea");
-
-    return log_area;
+    return document;
 }
 
 fn is_verbose_mode(src: &LogSources) -> bool {
